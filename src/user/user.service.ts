af6647dd62f35e21e4387355a970a5d8a44bdf40
M7MDdev1 <69204096+M7MDdev1@users.noreply.github.com>
2024-11-09 17:52:28 +0300

    Move createUser to services

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { User } from './entities/user.entity';
 
@@ -7,4 +7,22 @@ export class UserService {
   constructor(private readonly dataSource: DataSource) {}
   // repo global variable - (DB)
   userRepo = this.dataSource.getRepository(User);
+
+  async create(body) {
+    const user = new User();
+    const { fullName, age } = body;
+
+    if (!fullName || !age) {
+      throw new HttpException(
+        'Full name and age are required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    user.fullName = fullName;
+    user.age = age;
+
+    await this.userRepo.save(user);
+
+    return { message: 'User created Successfully', user: user };
+  }
 }
