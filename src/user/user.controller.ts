05d6cbbb6edc171cab5dc4e3318b8eee5f555077
Mohannad Ebrahim Alhatame <mr.m0ohannad@gmail.com>
2024-11-09 18:58:42 +0300

    get & delete user by id

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -60,25 +60,20 @@ export class UserController {
   // Get user by id
   @Get(':id')
   async findUser(@Param('id') id: number) {
-    // findone parameter should be an object, the condition is passed on "where: {condition}"
-    const user = await this.userRepo.findOne({ where: { id: id } });
-
-    return { user: user };
+    return this.userService.getUserById(id);
   }
 
   // Delete user by id
   @Delete(':id')
   async DeleteUser(@Param('id') id: number) {
-    // delete - Deletes entities by entity id, ids or given conditions:
-    this.userRepo.delete(id);
-
-    return 'User Deleted Successfully';
+    return this.userService.deleteUser(id);
   }
   // End Mohannad todo :
 
   // Faisal todo :
   // todo : delete array of users by id
   @Delete(':id/:id2/:id3')
+  // return this.userService.delete(body);
   async DeleteUsers(
     @Param('id') id: number,
     @Param('id2') id2: number,
